f3c58d900e635d157357692b6487b83745b75bde
Sem-Lord <semfanou@gmail.com>
2023-06-25 18:24:32 +0200

    Sécurisation des endpoint de l’api

---
+++ enfivy-back/src/main/java/com/envify/back/security/AuthenticationService.java
@@ -0,0 +1,37 @@
+package com.envify.back.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+/**
+ * @author semfa
+ *
+ */
+@Configuration
+public class AuthenticationService {
+	
+    public static Authentication getAuthentication(HttpServletRequest request) {
+    	
+    	//TODO get api key values from conf
+    	
+        String apiKey = request.getHeader("ENVIFY-API-Key");
+        if (apiKey == null || !apiKey.equals("R1rQpb6ZZQY2ViNIaC6X--")) {
+            throw new BadCredentialsException("Invalid API Key");
+        }
+        
+        List<GrantedAuthority> authorities = new ArrayList<>();
+    	authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+        return new ApiKeyAuthentication(apiKey, authorities);
+    }
+	
+}
