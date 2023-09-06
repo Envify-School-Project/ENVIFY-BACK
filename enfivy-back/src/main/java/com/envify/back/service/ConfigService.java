5a643b4a2a82f1d5903ccbe3a7d34acad56b8fff
PiRB <pierrebrivio14@gmail.com>
2023-09-06 11:45:21 +0200

    fixes

--- enfivy-back/src/main/java/com/envify/back/service/ConfigService.java
+++ enfivy-back/src/main/java/com/envify/back/service/ConfigService.java
@@ -1,9 +1,11 @@
 package com.envify.back.service;
 
 import com.envify.back.entity.ConfigEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface ConfigService {
     List<ConfigEntity> findAllConfigs();
     ConfigEntity findConfigById(int id);
