8258ebcddb1f9202411f698bc12f6916dd82e7f6
Sem-Lord <semfanou@gmail.com>
2023-09-07 16:05:18 +0200

    JWT authentication implementation

---
+++ enfivy-back/src/main/java/com/envify/back/dto/AuthRequest.java
@@ -0,0 +1,38 @@
+package com.envify.back.dto;
+
+import java.util.Objects;
+
+public class AuthRequest {
+	
+	private String email;
+	private String password;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, password);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AuthRequest other = (AuthRequest) obj;
+		return Objects.equals(email, other.email) && Objects.equals(password, other.password);
+	}
+}
