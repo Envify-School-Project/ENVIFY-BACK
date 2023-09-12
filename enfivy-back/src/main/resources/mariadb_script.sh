526e19c255c3af57267b2a8525d5e561eba74b98
Sem-Lord <semfanou@gmail.com>
2023-09-12 12:10:58 +0200

    Clean code

--- enfivy-back/src/main/resources/mariadb_script.sh
+++ enfivy-back/src/main/resources/mariadb_script.sh
@@ -16,10 +16,5 @@ echo "Enter current password for root (enter for none): Press enter as there is 
       Disallow root login remotely? [Y/n]: Enter Y \n
       Remove the test database and access to it? [Y/n]: Enter Y \n
       Reload privilege tables now? [Y/n]: Enter Y \n "
-#Enter current password for root (enter for none): Press enter as there is no password by default.
-#Set root password? [Y/n]: Select Y and enter a new password.
-#Remove anonymous users? [Y/n]: Select Y
-#Disallow root login remotely? [Y/n]: Enter Y
-#Remove the test database and access to it? [Y/n]: Enter Y
-#Reload privilege tables now? [Y/n]: Enter Y
+
 sudo mysql_secure_installation
