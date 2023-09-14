122d099962ab886e09ec1d24bc7abbbea20b7c48
Haris Souici <souici.haris@gmail.com>
2023-09-14 13:01:28 +0200

    FEATURE: Add ConfigPackageController

--- enfivy-back/src/main/java/com/envify/back/controller/ConfigPackageController.java
+++ enfivy-back/src/main/java/com/envify/back/controller/ConfigPackageController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/packages")
+@RequestMapping("/api/v1/config-packages")
 public class ConfigPackageController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigPackageController.class);
