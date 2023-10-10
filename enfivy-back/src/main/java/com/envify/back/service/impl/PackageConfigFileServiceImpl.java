dbd3b10b4575229582d9e1f168d9940858c2ff68
Sem-Lord <semfanou@gmail.com>
2023-10-10 07:04:31 +0200

    Add package config classes

---
+++ enfivy-back/src/main/java/com/envify/back/service/impl/PackageConfigFileServiceImpl.java
@@ -0,0 +1,27 @@
+package com.envify.back.service.impl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.envify.back.dao.PackageConfigFileDao;
+import com.envify.back.entity.PackageConfigFileEntity;
+import com.envify.back.service.PackageConfigFileService;
+
+@Service
+@Transactional
+public class PackageConfigFileServiceImpl implements PackageConfigFileService{
+	
+	@Autowired
+	private PackageConfigFileDao packageConfigFileDao;
+
+	@Override
+	public List<PackageConfigFileEntity> findByPackageVersionId(int packageVersionId) {
+		packageConfigFileDao.getByPackageVersionId(packageVersionId);
+		
+		return null;
+	}
+}
