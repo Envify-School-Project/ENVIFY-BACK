32ebf3218fd706d345008614a5f35c3b5f10dbe0
Sem-Lord <semfanou@gmail.com>
2023-10-11 14:38:49 +0200

    Get user information during authentication

--- enfivy-back/src/main/java/com/envify/back/dto/UserDto.java
+++ enfivy-back/src/main/java/com/envify/back/dto/UserDto.java
@@ -7,6 +7,10 @@ public class UserDto {
 	private long id;
 	private String email;
 	private String password;
+	private String lastName;
+	private String firstName;
+	private String username;
+	private String company;
 	
 	public long getId() {
 		return id;
@@ -26,9 +30,36 @@ public class UserDto {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(email, id, password);
+		return Objects.hash(company, email, firstName, id, lastName, password, username);
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -39,6 +70,9 @@ public class UserDto {
 		if (getClass() != obj.getClass())
 			return false;
 		UserDto other = (UserDto) obj;
-		return Objects.equals(email, other.email) && id == other.id && Objects.equals(password, other.password);
+		return Objects.equals(company, other.company) && Objects.equals(email, other.email)
+				&& Objects.equals(firstName, other.firstName) && id == other.id
+				&& Objects.equals(lastName, other.lastName) && Objects.equals(password, other.password)
+				&& Objects.equals(username, other.username);
 	}
 }
