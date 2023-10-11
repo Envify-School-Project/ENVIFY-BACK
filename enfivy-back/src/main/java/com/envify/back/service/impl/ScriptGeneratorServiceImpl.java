3da3ec5e6400ab2486717811187da59830b2ed51
Sem-Lord <semfanou@gmail.com>
2023-10-11 20:25:35 +0200

    Clean code

--- enfivy-back/src/main/java/com/envify/back/service/impl/ScriptGeneratorServiceImpl.java
+++ enfivy-back/src/main/java/com/envify/back/service/impl/ScriptGeneratorServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
