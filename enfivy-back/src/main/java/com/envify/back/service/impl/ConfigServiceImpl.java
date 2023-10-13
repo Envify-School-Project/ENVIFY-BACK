7c887da1370b166e2561a304780dc04744919ef5
Haris Souici <souici.haris@gmail.com>
2023-10-14 00:34:01 +0200

    [FEATURE] Add os name in config object

--- enfivy-back/src/main/java/com/envify/back/service/impl/ConfigServiceImpl.java
+++ enfivy-back/src/main/java/com/envify/back/service/impl/ConfigServiceImpl.java
@@ -3,8 +3,10 @@ package com.envify.back.service.impl;
 import com.envify.back.dao.ConfigDao;
 import com.envify.back.dao.UserDao;
 import com.envify.back.entity.ConfigEntity;
+import com.envify.back.entity.OperatingSystemEntity;
 import com.envify.back.entity.UserEntity;
 import com.envify.back.service.ConfigService;
+import com.envify.back.service.OperatingSystemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,8 @@ public class ConfigServiceImpl implements ConfigService {
     private ConfigDao configDao;
     @Autowired
     private UserDao userDao;
+    @Autowired
+    private OperatingSystemService operatingSystemService;
 
     @Override
     public List<ConfigEntity> findAllConfigs() {
@@ -45,15 +49,23 @@ public class ConfigServiceImpl implements ConfigService {
         configDao.deleteById(id); 
     }
 
+    private void setOperatingSystemName(ConfigEntity config) {
+        OperatingSystemEntity os = operatingSystemService.findOperatingSystemById(config.getOperatingSystemId());
+        config.setOperatingSystemName(os.getName());
+    }
+
     @Override
     public List<ConfigEntity> findConfigsByUserId(int userId) {
-        return configDao.findConfigsByUserId(userId);
+        List<ConfigEntity> configs = configDao.findConfigsByUserId(userId);
+
+        return configs.stream().peek(this::setOperatingSystemName).toList();
     }
 
     @Override
     public List<ConfigEntity> findConfigsByUserRole(String userRole) {
         UserEntity user = userDao.findByRole(userRole);
+        List<ConfigEntity> configs = configDao.findConfigsByUserId(user.getId());
 
-        return configDao.findConfigsByUserId(user.getId());
+        return configs.stream().peek(this::setOperatingSystemName).toList();
     }
 }
