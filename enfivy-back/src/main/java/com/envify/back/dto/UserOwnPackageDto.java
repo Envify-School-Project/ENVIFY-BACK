2609e2e3620b213621b75b20b7d9363d69242c23
Haris Souici <44004478+simtaxx@users.noreply.github.com>
2023-10-14 23:10:20 +0200

    [FEATURE] Add package version id (#51)

--- enfivy-back/src/main/java/com/envify/back/dto/UserOwnPackageDto.java
+++ enfivy-back/src/main/java/com/envify/back/dto/UserOwnPackageDto.java
@@ -5,10 +5,12 @@ import java.util.Objects;
 public class UserOwnPackageDto {
     private String name;
     private String versionNumber;
+    private Integer packageVersionId;
 
-    public UserOwnPackageDto(String name, String versionNumber) {
+    public UserOwnPackageDto(String name, String versionNumber, Integer packageVersionId) {
         this.name = name;
         this.versionNumber = versionNumber;
+        this.packageVersionId = packageVersionId;
     }
 
     public String getName() {
@@ -27,16 +29,26 @@ public class UserOwnPackageDto {
         this.versionNumber = versionNumber;
     }
 
+    public Integer getPackageVersionId() {
+        return packageVersionId;
+    }
+
+    public void setPackageVersionId(Integer packageVersionId) {
+        this.packageVersionId = packageVersionId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UserOwnPackageDto userOwnPackageDto = (UserOwnPackageDto) o;
-        return Objects.equals(name, userOwnPackageDto.name) && Objects.equals(versionNumber, userOwnPackageDto.versionNumber);
+        return Objects.equals(name, userOwnPackageDto.name)
+                && Objects.equals(versionNumber, userOwnPackageDto.versionNumber)
+                && Objects.equals(packageVersionId, userOwnPackageDto.packageVersionId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, versionNumber);
+        return Objects.hash(name, versionNumber, packageVersionId);
     }
 }
