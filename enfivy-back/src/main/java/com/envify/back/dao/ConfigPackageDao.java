7500b1c13997c0fd50e867877be7898810c55d63
Haris Souici <souici.haris@gmail.com>
2023-10-14 22:01:36 +0200

    [FEATURE] Update query

--- enfivy-back/src/main/java/com/envify/back/dao/ConfigPackageDao.java
+++ enfivy-back/src/main/java/com/envify/back/dao/ConfigPackageDao.java
@@ -1,10 +1,7 @@
 package com.envify.back.dao;
 
 import com.envify.back.entity.ConfigPackageEntity;
-import com.envify.back.entity.ConfigPackageIdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
