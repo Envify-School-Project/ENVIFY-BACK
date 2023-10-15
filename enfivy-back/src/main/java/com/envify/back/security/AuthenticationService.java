7d0655ab6118f5f304abe66c5804af74fa85c481
Sem-Lord <semfanou@gmail.com>
2023-10-15 17:22:57 +0200

    Double authentication implementation

--- enfivy-back/src/main/java/com/envify/back/security/AuthenticationService.java
+++ enfivy-back/src/main/java/com/envify/back/security/AuthenticationService.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.envify.back.exception.EnvifyException;
+
+import io.jsonwebtoken.Claims;
 
 /**
  * @author semfa
@@ -18,11 +23,21 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 @Configuration
 public class AuthenticationService {
 	
-    public static Authentication getAuthentication(HttpServletRequest request) {
+	private static JWTUtil jwtUtil;
+	
+	public AuthenticationService(JWTUtil jwtUtil) {
+		AuthenticationService.jwtUtil = jwtUtil;
+	}
+	
+	public static Authentication getAuthentication(HttpServletRequest request) throws EnvifyException {
+    	
+    	String accessToken = jwtUtil.resolveToken(request);
     	
     	//TODO get api key values from conf
         String apiKey = request.getHeader("ENVIFY-API-Key");
-        if (apiKey == null || !apiKey.equals("R1rQpb6ZZQY2ViNIaC6X--")) {
+        if (apiKey == null || !apiKey.equals("R1rQpb6ZZQY2ViNIaC6X--") 
+        		|| !jwtUtil.validateToken(accessToken)) {
+        	
             throw new BadCredentialsException("Invalid API Key");
         }
         
